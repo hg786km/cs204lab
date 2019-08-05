d28c7665368cf84c166fa79dfcc939c6069ed4c5
khiladimahan1210 <44144513+khiladimahan1210@users.noreply.github.com>
2019-08-05 03:56:33 -0400

    updated prime

--- lab1/prime.cpp
+++ lab1/prime.cpp
@@ -1,199 +1,234 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+string increment_one(string str){
 
-string Add_strings(string first, string second) 
-{ 
+	reverse(str.begin(),str.end());
 
-    if (first.length() > second.length()) 
-        swap(first, second); 
-    string final = ""; 
-    reverse(first.begin(), first.end()); 
-    reverse(second.begin(), second.end()); 
-    int carry = 0; 
-    for (int i=0; i<first.length(); i++) 
-    { 
-        int sum = ((first[i]-'0')+(second[i]-'0')+carry); 
-        final.push_back(sum%10 + '0'); 
-        carry = sum/10; 
-    } 
-    for (int i=first.length(); i<second.length(); i++) 
-    { 
-        int sum = ((second[i]-'0')+carry); 
-        final.push_back(sum%10 + '0'); 
-        carry = sum/10; 
-    }  
-    if (carry) 
-        final.push_back(carry+'0'); 
-    reverse(final.begin(), final.end()); 
-    return final; 
-} 
-bool is_small(string first, string second)
-{
-    int n1 = first.length(), n2 = second.length();
+	int n=str.size();
 
-    if (n1 < n2)
-    return true;
-    if (n2 < n1)
-    return false;
+	int carry=0;
 
-    for (int i=0; i<n1; i++)
-    if (first[i] < second[i])
-        return true;
-    else if (first[i] > second[i])
-        return false;
+	int sum=(str[0]-'0')+1;
 
-    return false;
+	carry=sum/10;
+
+	str[0]=(sum%10+'0');
+
+	for(int i=1;i<n;i++){
+		if(carry==0) 
+			{
+				reverse(str.begin(),str.end()); 
+				return str;
+			}
+		else{
+			sum=(str[i]-'0')+carry;
+			str[i]=(sum%10+'0');
+			carry=sum/10;
+		}
+	}
+	if(carry)
+		 str+=(carry+'0');
+	reverse(str.begin(),str.end());
+	return str;
 }
+bool is_small(string str1, string str2)
+{
+	int n1 = str1.length(), n2 = str2.length(); 
+
+	if (n1 < n2) 
+	return true; 
+	if (n2 < n1) 
+	return false; 
+
+	for (int i=0; i<n1; i++) 
+	if (str1[i] < str2[i]) 
+		return true; 
+	else if (str1[i] > str2[i]) 
+		return false; 
+
+	return false; 
+}
+bool is_small_or_equal(string str1, string str2) 
+{ 
+	int n1 = str1.length(), n2 = str2.length(); 
+
+	if (n1 < n2) 
+	return true; 
+	if (n2 < n1) 
+	return false; 
+
+	for (int i=0; i<n1; i++) 
+	if (str1[i] < str2[i]) 
+		return true; 
+	else if (str1[i] > str2[i]) 
+		return false; 
+
+	return true; 
+} 
+
 string subtract(string first, string second)
 {
-    if (is_small(first, second))
-        swap(first, second);
-    string final = "";
-    int n1 = first.length(), n2 = second.length();
-    reverse(first.begin(), first.end());
-    reverse(second.begin(), second.end());
-    int carry = 0;
-    for (int i=0; i<n2; i++)
-    {
-        int sub = ((first[i]-'0')-(second[i]-'0')-carry);
-        if (sub < 0)
-        {
-            sub = sub + 10;
-            carry = 1;
-        }
-        else
-            carry = 0;
+	if (is_small(first, second))
+		swap(first, second);
+	string final = "";
+	int n1 = first.length(), n2 = second.length();
+	reverse(first.begin(), first.end());
+	reverse(second.begin(), second.end());
+	int carry = 0;
+	for (int i=0; i<n2; i++)
+	{
+		int sub = ((first[i]-'0')-(second[i]-'0')-carry);
+		if (sub < 0)
+		{
+			sub = sub + 10;
+			carry = 1;
+		}
+		else
+			carry = 0;
 
-        final.push_back(sub + '0');
-    }
-    for (int i=n2; i<n1; i++)
-    {
-        int sub = ((first[i]-'0') - carry);
-        if (sub < 0)
-        {
-            sub = sub + 10;
-            carry = 1;
-        }
-        else
-            carry = 0;
-        final.push_back(sub + '0');
-    }
-    reverse(final.begin(), final.end());
-    return final;
+		final.push_back(sub + '0');
+	}
+	for (int i=n2; i<n1; i++)
+	{
+		int sub = ((first[i]-'0') - carry);
+		if (sub < 0)
+		{
+			sub = sub + 10;
+			carry = 1;
+		}
+		else
+			carry = 0;
+		final.push_back(sub + '0');
+	}
+	reverse(final.begin(), final.end());
+	return final;
 }
 
-bool greaterORequal(string s1, string s2){
-    if(s2.size() < s1.size())
-    {
-        return true;
-    }
-    else if((s2 <= s1) && (s2.size() == s1.size()))
-    {
-        return true;
-    }
-    return false;
-}
-string divide(string s1, string s2){
-    if(s2.size() > s1.size()){
-        string s=s1;
-        s1=s2;
-        s2=s;
-    }
-    else if(s2 > s1 && s2.size() == s1.size()){
-        string s=s1;
-        s1=s2;
-        s2=s;
-    }
-    string remainder="";
-    string answer="";
-    int j=0;
-    while(1){
-        string divisor=s2;
-        string dividend = remainder;
-        bool flag= false;
-        if(j == 0){
-            for(int i=j; i<s1.size(); i++){
-            dividend += s1[i];
-            if(greaterORequal(dividend, divisor)){
-                j = i+1;
-                flag = true;
-                break;
-                }
-            }
-        }
-        else{
-        for(int i=j; i<s1.size(); i++){
-            dividend += s1[i];
-            string sm="";
-            for(int k =0 ; k<dividend.size(); k++){
-                if(dividend[k] == '0' && sm == ""){
-                    continue;
-                }
-                else{sm += dividend[k];}
-            }
-            dividend = sm;
-            if(greaterORequal(dividend, divisor)){
-                j = i+1;
-                flag = true;
-                break;
-            }
-            answer += '0';
-        }}
-        if(dividend == ""){dividend = '0';}
 
-        if(!flag && !greaterORequal(dividend, divisor)){
 
-            return dividend;
-        }
-        int j2=0;
-        while(greaterORequal(dividend, divisor)){
-            divisor = Add_strings(divisor, s2);
-            j2++;
-        }
-        divisor = subtract(divisor, s2);
-        remainder = subtract(dividend, divisor);
-        if(j2 != 0){
-            answer += j2+'0';
-        }
+string division(string a , string b){
+	string ans="";
+	string zero="0";
 
-    }
+
+	if(is_small(a,b)) {
+		return zero;
+	}
+
+
+	string r=a.substr(0,b.size());
+
+	if(is_small(r,b)) {
+		r=r+a[b.size()];
+	}
+
+
+	int size1=r.size();
+
+	while(is_small_or_equal(b,r))
+	{
+
+		int i=0;
+
+		while(is_small_or_equal(b,r)){
+			
+			r=subtract(b,r);
+			
+			int count=0;
+			
+			for(int j=0;j<r.size();j++){
+				if(r[j]!='0') 
+					break;
+				else 
+					count++;
+			}
+			
+			if(count<r.size()) 
+				{r=r.substr(count,r.size()-count);}
+			else
+				{ r="";}
+			i++;
+		}
+
+		ans+=(i+'0');
+
+		if(size1>=a.size()) 
+			break;
+
+		r+=a[size1];
+
+		size1+=1;
+
+		int count=0;
+
+    	for(int j=0;j<r.size();j++){
+			if(r[j]!='0') 
+				break;
+			else 
+				count++;
+		}
+
+
+		if(count<r.size()) 
+			r=r.substr(count,r.size()-count);
+		else 
+			r="";
+
+
+		while(is_small(r,b)&&size1<a.size()){
+			r+=a[size1];
+			size1++;
+			ans+='0';
+			int count=0;
+			for(int j=0;j<r.size();j++){
+				if(r[j]!='0') break;
+				else count++;
+			}
+			if(count<r.size()) r=r.substr(count,r.size()-count);
+			else r="";
+		}
+		if(is_small(r,b)) ans+='0';
+	}
+	
+	if(r.size()==0) r="0";
+	return r;
 }
-bool OnlyGreater(string s1, string s2){
-    if(s2.size() < s1.size()){
-        return true;
-    }else if(s2 < s1 && s2.size() == s1.size()){
-        return true;
-    }
-    return false;
+
+bool prime_or_not(string a){
+	int n=a.size();
+
+	n=n/2+1;
+
+	string two="2";
+
+	while(two.size()<=(n/2+1)&&is_small(two,a)){
+
+		string r=division(a,two);
+
+		if(r=="0") 
+			return false;
+
+		two=increment_one(two);
+
+	}
+	return true;
 }
-void prime(string one){
-    string s = "2";
-    if(one == "2"){
-        cout<<"Prime\n";
-        return;
-    }
-    if(one == "3"){
-        cout<<"Prime\n";
-        return;
-    }
-    while(OnlyGreater(one, s)){
-        if(divide(one, s) == "0"){
-            cout<<"Not a Prime\n";
-            return;
-        }
-        if(s == "2"){
-        s = Add_strings(s, "1");}
-        else{s = Add_strings(s, "2");
-        }
-    }
-    cout<<"Prime\n";
-}
+
+
 int main(){
-    long long t;
-    cin>>t;
-    while(t--){
-        string s;
-        cin>>s;
-        prime(s);
-    }
+	int q;
+	cin>>q;
+	while(q--){
+
+		string str;
+		
+		cin>>str;
+
+
+		if(prime_or_not(str)) 
+			cout<<"Prime"<<endl;
+		else 
+			cout<<"Not a Prime"<<endl;
+	}
+	return 0;
 }
